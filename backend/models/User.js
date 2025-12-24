999b46f42d687ccb20506b8a6d0bdb47e406e47f
min0ihjdindifndinfdini <165413547+minigithubo@users.noreply.github.com>
2025-12-23 21:25:11 -0800

    chore: add gitignore and mongodb models

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+//platform(schema)
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// ⚠️ 여기 이름 정확히!
+module.exports = mongoose.model("User", UserSchema);
